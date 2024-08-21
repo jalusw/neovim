a9ee69c8b9fc09780c7a446f279d716536055b7c
Jalu Satriani Wijaya <jalusatrianiwijaya1@gmail.com>
2024-08-21 23:12:19 +0800

    init

---
+++ after/plugin/gruvbox.lua
@@ -0,0 +1,6 @@
+-- Default options:
+require("gruvbox").setup({
+	transparent_mode = true,
+})
+
+-- vim.cmd("colorscheme gruvbox")
---
+++ after/plugin/outline.lua
@@ -0,0 +1,3 @@
+require("outline").setup({})
+
+vim.keymap.set("n", "<leader>aa", "<cmd>Outline<cr>", { noremap = true })
